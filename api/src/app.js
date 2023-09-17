2b78c823b4e8a7ea0e40edaeca3744ea28ae551a
Eren Gül <erengul1502@gmail.com>
2023-09-17 16:14:44 +0300

    added try/catch block

--- api/src/app.js
+++ api/src/app.js
@@ -16,31 +16,33 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-app.get("/api/:brand", async (req, res) => { 
-  const browser = await puppeteer.launch({ headless: 'new' });
-  const page = await browser.newPage();
+app.get("/api/:brand", async (req, res) => {
+  try {
+    const browser = await puppeteer.launch({ headless: 'new' });
+    const page = await browser.newPage();
 
-  let complaintList = [];
-  for (let i = 1; i < 2; i++) {
+    let complaintList = [];
+    for (let i = 1; i < 2; i++) {
       await page.goto(`https://www.sikayetvar.com/${req.params.brand}?page=${i}`);
       const complaints = await page.$$eval('.card-v2', (elements) => elements.map((e) => {
-          if (e.querySelector('.complaint-hidden')) {
-              return;
-          }
-          return {
-              title: e.querySelector('.complaint-title').innerText,
-              username: e.querySelector('.username').innerText,
-              time: e.querySelector('.time').innerText,
-              description: e.querySelector('.complaint-description').innerText,
-              link: e.querySelector('.complaint-title a').href
-          };
+        if (e.querySelector('.complaint-hidden')) {
+          return;
+        }
+        return {
+          title: e.querySelector('.complaint-title').innerText,
+          username: e.querySelector('.username').innerText,
+          time: e.querySelector('.time').innerText,
+          description: e.querySelector('.complaint-description').innerText,
+          link: e.querySelector('.complaint-title a').href
+        };
       }));
       complaintList.push(complaints);
-  }
-  complaintList = complaintList.flat();
-  console.log(complaintList);
-  res.json(complaintList);
-  await browser.close();
+    }
+    complaintList = complaintList.flat();
+    console.log(complaintList);
+    res.json(complaintList);
+    await browser.close();
+  } catch (err) {}
 });
 
 app.use('/api/v1', api);
