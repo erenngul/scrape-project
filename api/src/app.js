b7b53add8387ece353fd871cb613d61294142a3e
Eren Gül <erengul1502@gmail.com>
2023-09-17 17:30:26 +0300

    changed args of puppeteer

--- api/src/app.js
+++ api/src/app.js
@@ -19,13 +19,14 @@ app.use(express.json());
 
 app.get("/api/:brand", async (req, res) => {
   try {
-    const browser = await chromium.puppeteer.launch({
-      args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
-      defaultViewport: chromium.defaultViewport,
-      executablePath: await chromium.executablePath,
-      headless: 'new',
-      ignoreHTTPSErrors: true
-    });
+    const browser = await puppeteer.launch({args: [
+      '--disable-gpu',
+      '--disable-dev-shm-usage',
+      '--disable-setuid-sandbox',
+      '--no-first-run',
+      '--no-sandbox',
+      '--no-zygote',
+  ]});
     const page = await browser.newPage();
 
     let complaintList = [];
